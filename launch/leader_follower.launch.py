fb368da244a257c4ed6901b422aae0533deda4c5
USTAT19 <singhustat9@gmail.com>
2026-01-14 19:54:38 +0530

    launch file edited

--- launch/leader_follower.launch.py
+++ launch/leader_follower.launch.py
@@ -3,14 +3,14 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        # 1) Turtlesim simulation
+        
         Node(
             package='turtlesim',
             executable='turtlesim_node',
             name='turtlesim'
         ),
 
-        # 2) Your bridge: /turtle1/pose → /leader_pose
+        
         Node(
             package='turtle_follower',
             executable='teleop_leader',
@@ -24,7 +24,7 @@ def generate_launch_description():
             output='screen'
         ),
 
-        # 3) Your follower controller (turtle2 follows leader_pose)
+        
         Node(
             package='turtle_follower',
             executable='follower',
